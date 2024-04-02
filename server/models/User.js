54bd0ab2216906cd8d3e9037c47728d318474e5a
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-01 21:20:09 -0700

    done with models

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,48 @@
-const User = require('./User');
-const Product = require('./Product');
-const Category = require('./Category');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
 const Order = require('./Order');
 
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  orders: [Order.schema]
+});
 
-module.exports = { User, Product, Category, Order };
+// Setting up password using preset middleware.
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// Comparing the password against input password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+// Export User
+module.exports = User;
