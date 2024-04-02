670f9624d559e64fefeb866ee3c24abdc3686ea6
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-02 14:03:09 -0700

    fixing packages

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,7 +1,7 @@
 
 
-const db = require('./config/connection');
-const { User, Product, Category } = require('./models');
+const db = require('./connection');
+const { User, Product, Category } = require('../models');
 
 db.once('open', async () => {
     await Category.deleteMany();
