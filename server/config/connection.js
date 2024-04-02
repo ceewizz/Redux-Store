1791b518d2fb27146458f09e2663613a89b5debc
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-02 15:02:44 -0700

    fixed bugged

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/amakonstore', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ceewizzhuhwin:rKSWtbGsIzff9G0f@amakonstore.mjb85gs.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   
