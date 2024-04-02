45e59da3152dfd5045f66da2ad78e79b87c6afe5
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-01 23:44:53 -0700

    bugged

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import emailjs from 'emailjs-com';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { BsTelephone } from 'react-icons/bs';
-import aboutImage from '../client/src/assets/about.png';
+import aboutImage from '..client/src/assets/about.png';
 
 const Contact = () => {
   const {
