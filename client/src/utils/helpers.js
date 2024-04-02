52fa9d769cee5b1dfb56c28fe9b9174bda07764b
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-02 03:34:23 -0700

    bugged

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -7,7 +7,7 @@ export function pluralize(name, count) {
   
   export function idbPromise(storeName, method, object) {
     return new Promise((resolve, reject) => {
-      const request = window.indexedDB.open('cruise-tour', 1);
+      const request = window.indexedDB.open('amakonstore', 1);
       let db, tx, store;
       request.onupgradeneeded = function(e) {
         const db = request.result;
