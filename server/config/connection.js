9ce952f06fe62e5f2f3cc8263c4e25acd54fb902
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-03 17:31:31 -0700

    reloading packages

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/amakonstore', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/amakon-store', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   
