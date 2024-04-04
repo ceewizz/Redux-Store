5f18573ddcd6674120a8115e945169af15654374
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-03 17:49:49 -0700

    reloading packages

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -7,7 +7,7 @@ export function pluralize(name, count) {
   
   export function idbPromise(storeName, method, object) {
     return new Promise((resolve, reject) => {
-      const request = window.indexedDB.open('amakonstore', 1);
+      const request = window.indexedDB.open('amakon-store', 1);
       let db, tx, store;
       request.onupgradeneeded = function(e) {
         const db = request.result;
