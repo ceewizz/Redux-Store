9020e95dd88a259f0eb246f3dd9e30afe313e63c
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-04 05:56:27 -0700

    packages

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,11 +2,13 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/redux-store', {
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  
+  useCreateIndex: true,
+  useUnifiedTopology: true
 
 });
+
 
 module.exports = mongoose.connection;
--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,5 @@
 
-const db = require('./connection');
+const db = require('./db');
 const { User, Product, Category } = require('../models');
 
 db.once('open', async () => {
