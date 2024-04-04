69af142f2bb92e2f899419ebd9dc23be4dade71a
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-03 17:16:47 -0700

    packages fix

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -21,92 +21,92 @@ db.once('open', async () => {
 
     const products = await Product.insertMany([
         {
-            name: 'desktop',
-            category: categories[0]._id,
+            name: 'Desktop G445',
             description: 'PowerSpec G445 Gaming PC Intel Core i9 12th Gen 12900K 3.2GHz Processor; NVIDIA GeForce RTX 4070 12GB GDDR6X; 32GB DDR4-3200 RAM; 1TB Solid State Drive',
-            image: 'desktop.png',
+            image: 'desktop.jpg',
+            category: categories[0]._id,
             price: 1499.00,
-            quantity: 20
+            quantity: 100
         },
         {
-            name: 'desktop1',
-            category: categories[0]._id,
+            name: 'Desktop Y60',
             description: 'IBuyPower Y60 Gaming PC Intel Core i9 14th Gen 14900KF 2.4GHz Processor; NVIDIA GeForce RTX 4070 Ti SUPER 16GB GDDR6X; 32GB DDR5-5600 RAM; 2TB Solid State Drive',
-            image: 'desktop1.png',
+            image: 'desktop1.jpg',
+            category: categories[0]._id,
             price: 1999.00,
-            quantity: 20
+            quantity: 100
 
         },
         {
             name: 'Lenovo Legion Pro 5 16" Gaming Laptop Computer Platinum Collection - Onyx Grey',
-            category: categories[1]._id,
             description: 'Intel Core i7 13th Gen 13700HX 1.5GHz Processor; NVIDIA GeForce RTX 4070 8GB GDDR6; 32GB DDR5-4800 RAM; 1TB Solid State Drive',
-            image: 'laptop.jpeg',
+            image: 'laptop.jpg',
+            category: categories[1]._id,
             price: 1199.00,
             quantity: 40
 
         },
         {
             name: 'Acer Predator Helios 16 PH16-71-91FL 16.0" Gaming Laptop Computer - Abyssal Black',
-            category: categories[1]._id,
             description: 'Intel Core i9 13th Gen 13900HX 1.6GHz Processor; NVIDIA GeForce RTX 4080 12GB GDDR6; 32GB DDR5-5600 RAM; 2TB Solid State Drive',
             image: 'laptop1.jpeg',
+            category: categories[1]._id,
             price: 1899.00,
             quantity: 30
         },
         {
             name: 'Samsung Odyssey G9 S49CG954 49" 5K DQHD (5120 x 1440) 240Hz Curved Screen Monitor',
-            category: categories[2]._id,
             description: 'AMD FreeSync Premium Pro; HDR; HDMI DisplayPort; CoreSync RGB Light',
             image: 'monitor.jpeg',
+            category: categories[2]._id,
             price: 1199.00,
             quantity: 40  
         },
         {
             name: 'Gigabyte AORUS CO49DQ SA 49" 5K WQHD (5210 x 1440) 144Hz Ultrawide Curved Screen Gaming Monitor',
-            category: categories[2]._id,
             description: 'AMD FreeSync Premium Pro; HDR; HDMI DisplayPort USB Type-C; Dashboard',
             image: 'monitor1.jpg',
+            category: categories[2]._id,
             price: 1099.00,
             quantity: 40   
         },
         {
             name: 'Logitech Keyboard and Mouse Combo',
-            category: categories[3]._id,
             description: 'Logitech MX Keys S Combo Low-Profile Wireless Keyboard + MX Master 3S Wireless Mouse - (Black)',
             image: 'kbmouse.jpg',
+            category: categories[3]._id,
             price: 199.00,
             quantity: 50   
         },
         {
             name: 'Cherry Keyboard and Mouse Combo',
-            category: categories[3]._id,
             description: 'Cherry DW 9500 Slim Wireless Keyboard and Mouse Combo - Black',
-            image: 'kbmouse1.jpeg',
+            image: 'kbmouse1.jpg',
+            category: categories[3]._id,
             price: 119.00,
             quantity: 50   
         },
         {
             name: 'Computer Speakers',
-            category: categories[4]._id,
             description: 'Edifier R1280T 2 Channel Stereo Powered Bookshelf Computer Speakers - Wood',
-            image: 'speaker.jpeg',
+            image: 'speaker.jpg',
+            category: categories[4]._id,
             price: 119.00,
             quantity: 50  
         },
         {
             name: 'INTEL Bundle',
-            category: categories[4]._id,
             description: 'Intel Core i9-12900K, ASUS Z790-V Prime WiFi DDR5, G.Skill Ripjaws S5 32GB Kit DDR5 6000, Computer Build Bundled',
-            image: 'intel.jpeg',
+            image: 'intel.jpg',
+            category: categories[4]._id,
             price: 399.00,
             quantity: 20
         },
         {
             name: 'AMD Bundle',
-            category: categories[4]._id,
             description: 'AMD Ryzen 7 7800X3D, Gigabyte B650 Gaming X AX v2, G.Skill Flare X5 Series 32GB DDR5-6000 Kit, Computer Build Bundle',
-            image: 'amd.png',
+            image: 'amd.jpg',
+            category: categories[4]._id,
             price: 499.00,
             quantity: 20
         }
@@ -119,10 +119,11 @@ db.once('open', async () => {
         firstName: 'Beta',
         lastName: 'b',
         email: 'bBeta@testmail.com',
-        password: 'password12707',
+        password: 'password11223',
         orders: [
             {
-                products: [products[0]._id, products[0]._id, products[1]._id]}
+                products: [products[0]._id, products[0]._id, products[1]._id]
+            }
         ]
     });
 
@@ -130,7 +131,7 @@ db.once('open', async () => {
         firstName: 'Alpha',
         lastName: 'Al',
         email: 'aalpha@testmail.com',
-        password: 'password12707'
+        password: 'password11223'
 
 
     });
