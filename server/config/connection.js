078f5882898866a38e5f245530f0fbace2530e13
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-03 23:23:06 -0700

    new connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/amakon-store', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/redux-store', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   
