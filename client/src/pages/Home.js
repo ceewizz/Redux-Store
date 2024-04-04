9575ff7e2ba69d675e06c51ee1b6d42d891813c1
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-04 06:18:16 -0700

    reloaded

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,12 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
-import Slider from "../components/Slider";
 import Footer from "../components/Footer";
 
 
 const Home = () => {
     return (
       <div className="container">
-        <Slider/>
         <CategoryMenu />
         <ProductList />
         <Cart />
