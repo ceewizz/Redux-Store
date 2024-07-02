30eaee83cbda61e9366d4f053beaab88fa5b876e
ceewizz <ceewizzhuhwin@gmail.com>
2024-07-01 19:15:44 -0700

    updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -68,7 +68,7 @@ const resolvers = {
         const price = await stripe.prices.create({
           product: product.id,
           unit_amount: products[i].price * 100,
-          currency: 'AUD',
+          currency: 'USD',
         });
 
         line_items.push({
